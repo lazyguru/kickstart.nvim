8c4f17bfb71bf6e0ce25558a06de7a62e8dbb8a1
Joe Constant <joe@joeconstant.com>
2024-04-04 00:01:51 -0600

    feat(init.lua): update nvim-treesitter ensure_installed languages
    Add 'go', 'python' to the list of languages ensured for installation by nvim-treesitter. This enhances syntax highlighting and language support.

--- init.lua
+++ init.lua
@@ -844,7 +844,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'python' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
