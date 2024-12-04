f55f29993f9b9b14ef883e9ea64ede9643de0f18
Joe Constant <joe.constant@klarna.com>
2024-12-04 10:55:57 -0700

    feat(obsidian-nvim.lua): update plugin configuration for specific file events
    - Remove filetype restriction to allow plugin loading beyond markdown files.
    - Introduce event-driven loading for files within a specific path, enhancing efficiency.
    - Simplify workspace configuration by removing predefined workspace paths, allowing for a more flexible setup.

--- lua/custom/plugins/obsidian-nvim.lua
+++ lua/custom/plugins/obsidian-nvim.lua
@@ -1,28 +1,25 @@
--- conditionally load plugin if /Applications/Obsidan.app is present
 if vim.fn.executable '/Applications/Obsidian.app' then
+  -- conditionally load plugin if /Applications/Obsidan.app is present
   return {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
-    ft = 'markdown',
+    -- ft = 'markdown',
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    --   "BufReadPre path/to/my-vault/**.md",
-    --   "BufNewFile path/to/my-vault/**.md",
-    -- },
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      'BufReadPre '
+        .. vim.fn.expand '~'
+        .. '/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes/**.md',
+      'BufNewFile ' .. vim.fn.expand '~' .. '/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes/**.md',
+    },
     dependencies = {
       -- Required.
       'nvim-lua/plenary.nvim',
     },
     opts = {
-      workspaces = {
-        {
-          name = 'notes',
-          path = '~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes',
-        },
-      },
+      workspaces = {},
     },
   }
 end
